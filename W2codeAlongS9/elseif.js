a692e2341370195b045efc73ae78b2c6ac95ffbe
D-Hornsby <denahornsby@yahoo.com>
2020-12-01 11:31:29 -0500

    else.if

---
+++ W2codeAlongS9/elseif.js
@@ -0,0 +1,18 @@
+// Exercise - code along mod1 week 4 day 1 slide 9. Nested if/else statement
+
+var num = 50
+
+if (num > 0 && num > 100) {
+	console.log('num is positive and greater then 100');
+}
+
+else if (num > 0 && num < 100){
+	console.log('if num is positive and less then 100');
+}
+
+if (num < 0){
+	console.log('if num is negitive');
+  }
+
+
+// terminal input
